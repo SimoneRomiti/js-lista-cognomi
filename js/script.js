86e5a1be41829a325dd3b081a8cfbbee6157bc5d
Simone Romiti <sr1992ei@gmail.com>
2020-11-16 18:20:47 +0100

    Controlli per cognome vuoto/numero/spazio/uguale a già presente

--- js/script.js
+++ js/script.js
@@ -10,26 +10,57 @@ pulsante.addEventListener("click",
 function(){
   // SALVATAGGIO INPUT
   var cognomeUtente = document.getElementById("input").value;
-  // TRASFORMA PRIMA LETTERA IN MAIUSCOLA
-  cognomeUtente = cognomeUtente[0].toUpperCase() + cognomeUtente.substring(1);
 
-  // AGGIUNTA COGNOME SU ARRAY
-  cognomi.push(cognomeUtente);
+  // ELIMINA SPAZI PRIMA E DOPO LA STRINGA
+  cognomeUtente = cognomeUtente.trim();
 
-  // ORDINAMENTO ALFABETICO ARRAY
-  var ordine = cognomi.sort();
+  // CONTROLLO COGNOME VUOTO/NUMERO
+  if(cognomeUtente == "" || isNaN(cognomeUtente) == false){
+    alert("Inserisci un cognome!")
+  } else{
+    // TRASFORMA TUTTA LA STRINGA IN MINUSCOLO E POI LA PRIMA LETTERA IN MAIUSCOLO
+    cognomeUtente = cognomeUtente.toLowerCase();
+    cognomeUtente = cognomeUtente[0].toUpperCase() + cognomeUtente.substring(1);
 
-  // SCRITTURA LISTA IN ORDINE ALFABETICO
-  for(var i = 0; i < ordine.length; i++){
-    document.getElementById("surname").innerHTML += "<li>" + ordine[i] + "</li>";
+    // CONTROLLO COGNOME GIA PRESENTE
+    var i = 0;
+    var trovato = false;
+    while(i < cognomi.length && trovato == false){
+      if(cognomeUtente != cognomi[i]){
+        i++;
+      } else{
+        trovato = true;
+        i++;
+      }
+    }
+
+    if(trovato == true){
+      alert("Cognome già presente, inserisci un cognome non presente");
+      cognomeUtente = "";
+    } else{
+      // SE COGNOME NON PRESENTE
+
+      // AGGIUNTA COGNOME SU ARRAY
+      cognomi.push(cognomeUtente);
+
+      // ORDINAMENTO ALFABETICO ARRAY
+      var ordine = cognomi.sort();
+
+      // SCRITTURA LISTA IN ORDINE ALFABETICO
+      for(var i = 0; i < ordine.length; i++){
+        document.getElementById("surname").innerHTML += "<li>" + ordine[i] + "</li>";
+      }
+      document.getElementById("surname").innerHTML += "<br>"
+
+      // SALAVATAGGIO E SCRITTURA POSIZIONE NUOVO COGNOME
+      var i = 0;
+      while(ordine[i] != cognomeUtente){
+        i++;
+      }
+      document.getElementById("position").innerHTML = "Il cognome inserito si trova in posizione: " + (i + 1);
+    }
   }
-  document.getElementById("surname").innerHTML += "<br>"
 
-  // SALAVATAGGIO E SCRITTURA POSIZIONE NUOVO COGNOME
-  var i = 0;
-  while(ordine[i] != cognomeUtente){
-    i++;
-  }
-  document.getElementById("position").innerHTML = "Il cognome inserito si trova in posizione: " + (i + 1);
+
 }
 );
